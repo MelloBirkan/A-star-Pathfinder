7d7df477031fef0ae07f876b9c57a3cac7a298b6
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-01-01 23:03:25 -0300

    Add printBoard function to display the game board
    Introduced a utility function `printBoard` in `main.cpp` to display the board's content in a structured format. Updated the main function to call `printBoard` for clearer debugging and visualization. Additionally, minor updates to project files related to build configurations and VCS mappings were included.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 
 using std::vector;
 
+auto printBoard(const vector<vector<int>>& board) -> void {
+  for (const auto& row: board) {
+    for (const auto& cell: row) {
+      std::cout << cell << " ";
+    }
+    std::cout << "\n";
+  }
+}
+
 auto main() -> int {
   vector<vector<int>> board = {
     {0, 1, 0, 0, 0, 0},
@@ -12,5 +21,7 @@ auto main() -> int {
  {0, 0, 0, 0, 1, 0}
   };
 
+
+  printBoard(board);
   return 0;
 }
