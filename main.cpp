6690ded4897e8bbe858d5b6fa0957c3ab6d904eb
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-01-01 22:25:21 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+auto main() -> int {
+  vector<vector<int>> board = {
+    {0, 1, 0, 0, 0, 0},
+ {0, 1, 0, 0, 0, 0},
+ {0, 1, 0, 0, 0, 0},
+ {0, 1, 0, 0, 0, 0},
+ {0, 0, 0, 0, 1, 0}
+  };
+
+  return 0;
+}
